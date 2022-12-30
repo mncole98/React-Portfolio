1e46a182fdd15a5d254c8e35d35b093e4c4bd578
Matthew Cole <matthewcole@Matthews-MacBook-Pro.local>
2022-12-30 14:52:49 -0800

    adding footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footer-div">
+      <footer>
+        <ul>
+          <a href="https://github.com/mncole98" target="blank">
+            <li>
+              <FaGithub />
+            </li>
+          </a>
+          <a href="https://www.linkedin.com" target="blank">
+            <li>
+              <FaLinkedin />
+            </li>
+          </a>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
