b627e71f7cc64260de43368084b1b2fb7dc10348
Matthew Cole <matthewcole@Matthews-MacBook-Pro.local>
2023-01-02 19:14:39 -0800

    Updates

---
+++ precache-manifest.7b4aa5c1250e53fd79c8224b0d9a59a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcc98e422393700dd7a4c625a9bb63d",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "cf95b923fdd503ae7e31",
+    "url": "/ReactPortfolio/static/css/main.e02e9a9d.chunk.css"
+  },
+  {
+    "revision": "f67bdf0e3e683baf0a4d",
+    "url": "/ReactPortfolio/static/js/2.2206059d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ReactPortfolio/static/js/2.2206059d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf95b923fdd503ae7e31",
+    "url": "/ReactPortfolio/static/js/main.70a6deae.chunk.js"
+  },
+  {
+    "revision": "34ee1c39492704da870d",
+    "url": "/ReactPortfolio/static/js/runtime-main.c857b1cd.js"
+  },
+  {
+    "revision": "2541cc282e0dfa8d416ab91b1e34b8fa",
+    "url": "/ReactPortfolio/static/media/cork-board.2541cc28.png"
+  },
+  {
+    "revision": "a71bceac91efd235755d4c46884926bd",
+    "url": "/ReactPortfolio/static/media/matt.a71bceac.jpeg"
+  }
+]);
